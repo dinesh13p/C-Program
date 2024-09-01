e2d3d618028b2cf685bd9ced34f6fe9243bfe1e4
dinesh13p <159114512+dinesh13p@users.noreply.github.com>
2024-09-01 13:52:34 +0545

    Initial commit

---
+++ TestFiles/Exam-I/Divisible-by-3-but-not-by-5.c
@@ -0,0 +1,16 @@
+//WAP to display sum of numbers from 1-100 that are divisible by 3 but not by 5.
+#include <stdio.h>
+int main()
+{
+    int i;
+    int sum = 0;
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 != 0)
+        {
+            sum += i;
+        }
+    }
+    printf("sum is %d", sum);
+    return 0;
+}
