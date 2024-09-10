5ba0abcc0589fc15ade806c2fb546265d4c530b5
dinesh13p <159114512+dinesh13p@users.noreply.github.com>
2024-09-10 08:37:41 +0545

    Commit

---
+++ Class-work/Bubble-sort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    int numbers[5] = {640, 34, 54, 112, 22}; // Array to be sorted
+    int size = 5;                            // Size of the array
+    int temp;                                // Temporary variable for the swapping
+    int i, j;
+
+    // Bubble Sort Algorithm
+    for (i = 0; i < size - 1; i++)
+    {
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (numbers[j] > numbers[j + 1])
+            {
+                // swapp the numbers[j] and numbers[j + 1]
+                temp = numbers[j];
+                numbers[j] = numbers[j + 1];
+                numbers[j + 1] = temp;
+            }
+        }
+    }
+
+    // Printing the sorted array
+    printf("Sorted array in ascending order: \n");
+    for (i = 0; i < size; i++)
+    {
+        printf("%d\t", numbers[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
